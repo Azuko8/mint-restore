de4c20b232646d56035140fb0accd0750d299df5
Azuko8 <andreimncalex@posteo.com>
2026-09-23 18:09:42 +0300

    Update mint-restore structure and packages to match fedora-restore

--- mint-restore.sh
+++ mint-restore.sh
@@ -1,30 +1,53 @@
 #!/usr/bin/env bash
-# mint only
-# signal, element, osu, vesktop, pear-desktop will be only installed thru their native packages on their site.
-echo "OS : Linux Mint"
-echo "Remember to install:"
-echo " - Signal"
-echo " - osu!"
-echo " - Vesktop"
-echo " - ytm"
-echo " - librewolf"
 set -euo pipefail
 
-echo "installing apt packages"
+echo "Linux Mint restore"
+echo
+echo "Remember to install manually:"
+echo "  - osu!"
+echo "  - Vesktop"
+echo "  - ytm"
+echo
+echo "Installing APT packages..."
 
+sudo apt update > /dev/null
 sudo apt install -y \
     git \
     fish \
     steam \
     micro \
-    obs-studio
+    obs-studio \
+    curl \
+    gpg \
+    extrepo > /dev/null
 
-echo "checking for flathub source"
+echo "==> Adding LibreWolf repository..."
+
+sudo extrepo enable librewolf > /dev/null
+sudo extrepo update librewolf > /dev/null
+
+echo "Adding Signal repository..."
+
+curl -fsSL https://updates.signal.org/desktop/apt/keys.asc \
+    | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+
+curl -fsSL https://updates.signal.org/static/desktop/apt/signal-desktop.sources \
+    | sudo tee /etc/apt/sources.list.d/signal-desktop.sources > /dev/null
+
+echo "Installing LibreWolf and Signal..."
+
+sudo apt update > /dev/null
+sudo apt install -y \
+    librewolf \
+    signal-desktop > /dev/null
+
+echo "Checking Flathub..."
 
 flatpak remote-add --if-not-exists flathub \
-https://dl.flathub.org/repo/flathub.flatpakrepo
+    https://dl.flathub.org/repo/flathub.flatpakrepo > /dev/null
 
-echo "installing flatpaks"
+echo "Installing Flatpaks..."
 
 flatpak install -y flathub \
     org.keepassxc.KeePassXC \
@@ -32,6 +55,7 @@ flatpak install -y flathub \
     net.davidotek.pupgui2 \
     org.prismlauncher.PrismLauncher \
     io.github.debasish_patra_1987.linuxthemestore \
-    org.localsend.localsend_app 
+    org.localsend.localsend_app > /dev/null
 
-echo "don't nuke this system in 2h."
+echo
+echo "Restore complete."
